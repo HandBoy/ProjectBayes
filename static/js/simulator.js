9bd966680aa43a5927bbbad7fb13ddbf8919b909
handerson <handersonmedeiros@gmail.com>
2017-02-27 13:57:32 -0300

    refactoring code forwar simulador.

--- static/js/simulator.js
+++ static/js/simulator.js
@@ -48,22 +48,18 @@ $(document).ready(function(){
         if(toten >= logs.length){
             alert("Chegou ao fim da simulação");
         } else {
-            var trs = logs[toten].children;
+            addValueSimulator(toten);
+        }
+    });
 
+    function addValueSimulator(valor){
+        var trs = logs[valor].children;
             if(trs[0].childNodes[0].textContent == "erro"){
-                console.log("deu erro");
                 $("#numeroErrado"+trs[1].childNodes[0].textContent).text(trs[2].childNodes[0].textContent);
                 $("#numeroErrado"+trs[1].childNodes[0].textContent).addClass("numberError")
             } else if(trs[0].childNodes[0].textContent == "acerto"){
-                console.log("deu erro");
                 $("#numero"+trs[1].childNodes[0].textContent).text(trs[2].childNodes[0].textContent);
                 $("#numero"+trs[1].childNodes[0].textContent).addClass("numberRight")
             }
-        }
-
-
-
-    });
-
-
+    }
 });
