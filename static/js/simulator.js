26a80fa1cf97bfc5b1952c6e6bee0ec0b09784ef
handerson <handersonmedeiros@gmail.com>
2017-02-27 14:22:42 -0300

    refactoring code backward simulador.

--- static/js/simulator.js
+++ static/js/simulator.js
@@ -2,30 +2,7 @@
  * Created by gamedu on 23/02/17.
  */
 
-
-//var linha = document.getElementById("myTable").rows[0].cells.namedItem("inicio");
-
-
 $(document).ready(function(){
-
-    /*$('#inicio:first-child').each(function() {
-     var td = $(this).children('td:eq(3)');
-     console.log(td.text());
-     });*/
-    var filhos = $('#inicio:first-child');
-    //console.log(filhos.children('td:eq(3)').text())
-    var valores = filhos.children('td:eq(3)').text();
-    var valor = valores.split(";");
-    //console.log(valor.length);
-    for (var i=0;i<valor.length;i++){
-        //console.log(valor[i]);
-        $("#numero"+valor[i]).text(valor[i]);
-    }
-    $('tr').each(function() {
-        //var td = $(this).children('td:eq(3)');
-        // console.log($(this).html());
-    });
-
     var logs = $('tbody > tr:not([id])');
 
     var toten = -1;
